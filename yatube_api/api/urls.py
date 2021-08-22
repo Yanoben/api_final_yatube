08ce99073a078afcedd3e59b25a58193d23f2fb2
Ghost <yasinabdurakhmanov@gmail.com>
2021-08-22 19:30:35 +0300

    changes in urls

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -4,13 +4,13 @@ from rest_framework.authtoken import views
 from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
-router.register('v1/posts', PostViewSet, basename='post')
-router.register('v1/groups', GroupViewSet, basename='group')
-router.register(r'v1/posts/(?P<post_id>\d+)/comments',
+router.register('posts', PostViewSet, basename='post')
+router.register('groups', GroupViewSet, basename='group')
+router.register(r'posts/(?P<post_id>\d+)/comments',
                 CommentViewSet, basename='comments')
-router.register('v1/follow', FollowViewSet, basename='follow')
+router.register('follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('v1/', include(router.urls)),
     path('v1/api-token-auth/', views.obtain_auth_token),
 ]
